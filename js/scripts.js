864b5762cd1b3901233cb67578aa089cb673ab3f
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-14 11:11:03 -0700

    Add JS. Determine const.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+$("#formSurvey").submit(function(event) {
+  event.preventDefault();
+  const music = ($("input:radio[name=music]:checked").val());
+  const color = ($("input:radio[name=color]:checked").val());
+  const pet = ($("input:radio[name=pet]:checked").val());
+  const clothing = ($("input:radio[name=clothing]:checked").val());
+  const planet = ($("input:radio[name=planet]:checked").val());
+
+
+
+  
+  });
+});
